4825f06a1313a0b2930ba5e3a3295663b2dc719c
almalee24 <flores.almalee@gmail.com>
2020-11-16 12:42:59 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let array = [ 'Lisa', 'Kaitlin', 'Jan']
+let event = ['surprise']
+function writeCards(array, event) {
+    let results = []
+    for(let i = 0; i < array.length; i++){
+        results.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return results;
+}
+
+let num = 10
+function countDown(num) {
+    while(num >= 0){
+        console.log(num--)
+    }
+}
